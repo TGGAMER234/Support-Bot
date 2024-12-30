b8027322b77e5db890ef82ebef036186cd3c2284
TGGAMER234 <89134204+TGGAMER234@users.noreply.github.com>
2024-12-30 18:32:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    modmail: {
+        ownerID: '600342376338292756', // ← Insert the Server Owner ID.
+        supportId: '1323268442194841693', // ← Insert General Support Category ID.
+        premiumId: '1323268471382872156', // ← Insert Premium Support Category ID.
+        whitelist: '1323268597040283679', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1323268640258134036', // ← Insert Staff Role to be pinged upon ticket creation.
+    },
+    logs: {
+        logschannel: '1323268573094875180' // ← Insert Ticket Logs Channel ID.
+    }
+};
